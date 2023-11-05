b444496c802f3f8c54ae125175ed98e246e6cc3a
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-11-04 22:28:26 -0500

    test(eCommerce): testing navigability and product selection

---
+++ Guru99/src/test/java/eCommerce/Guru99/AppTest.java
@@ -0,0 +1,72 @@
+package eCommerce.Guru99;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	private WebDriver driver;
+	public String url;
+	public int scc = 0;
+
+	@BeforeTest
+	public void setUp() {
+		driver = new FirefoxDriver();
+		url = "http://live.techpanda.org/";
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+	}
+
+	@Test(priority = 0)
+	public void verifyTitleHome() {
+
+		String titleHome = "THIS IS DEMO SITE FOR   ";
+		String titleHomePage = driver.findElement(By.tagName("h2")).getText();
+
+		Assert.assertEquals(titleHomePage, titleHome);
+	}
+
+	@Test(priority = 1)
+	public void navegateMobilePage() {
+		driver.findElement(By.linkText("MOBILE")).click();
+	}
+
+	@Test(priority = 2)
+	public void verifyTitleMobile() {
+		String titleMobile = "MOBILE";
+		String titleMobilePage = driver.findElement(By.tagName("h1")).getText();
+
+		Assert.assertEquals(titleMobilePage, titleMobile);
+	}
+
+	@Test(priority = 3)
+	public void selectProductByName() {
+		new Select(driver.findElement(By.cssSelector("select[title=\"Sort By\"]"))).selectByVisibleText("Name");
+	}
+
+	@Test(priority = 4)
+	public void sortForProductName() throws Exception {
+		String EVIDENCE_PATH = "..\\Guru99\\evidence\\";
+		scc = (scc + 1);
+
+		File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String png = (EVIDENCE_PATH + scc + ".png");
+		FileUtils.copyFile(screen, new File(png));
+	}
+	
+	@AfterTest
+	public void close() {
+		driver.close();
+	}
+}
